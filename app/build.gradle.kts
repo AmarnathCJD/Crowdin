f31df4c212d642c448c722e8b48b3a05f2dd1278
AmarnathCJD <amarnathcharichilil@gmail.com>
2024-08-18 01:09:50 +0530

    support android 12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.crowdin"
-        minSdk = 33
+        minSdk = 32
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
